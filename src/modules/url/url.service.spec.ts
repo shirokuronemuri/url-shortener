79011479ca686b308eae4d42d366595c3e421809
Ihor Pavliuk <akira.pavl@gmail.com>
2026-01-07 01:15:07 +0200

    started working on url service tests

--- src/modules/url/url.service.spec.ts
+++ src/modules/url/url.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UrlService } from './url.service';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { DatabaseService } from 'src/services/database/database.service';
+import { IdGeneratorService } from 'src/services/id-generator/id-generator.service';
+import { TypedConfigService } from 'src/config/typed-config.service';
+import { RedisService } from 'src/services/redis/redis.service';
 
 describe('UrlService', () => {
-  let service: UrlService;
+  let urlService: UrlService;
+
+  let db: DeepMockProxy<DatabaseService>;
+  let idGenerator: DeepMockProxy<IdGeneratorService>;
+  let config: DeepMockProxy<TypedConfigService>;
+  let redis: DeepMockProxy<RedisService>;
 
   beforeEach(async () => {
+    db = mockDeep<DatabaseService>();
+    idGenerator = mockDeep<IdGeneratorService>();
+    config = mockDeep<TypedConfigService>();
+    redis = mockDeep<RedisService>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UrlService],
+      providers: [
+        UrlService,
+        {
+          provide: DatabaseService,
+          useValue: db,
+        },
+        {
+          provide: IdGeneratorService,
+          useValue: idGenerator,
+        },
+        {
+          provide: TypedConfigService,
+          useValue: config,
+        },
+        {
+          provide: RedisService,
+          useValue: redis,
+        },
+      ],
     }).compile();
 
-    service = module.get<UrlService>(UrlService);
+    urlService = module.get<UrlService>(UrlService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  it('should be defined', () => {});
 });
