7d5e429345676436d4c8319fafea9c636584c10b
Ihor Pavliuk <akira.pavl@gmail.com>
2026-01-10 02:21:14 +0200

    Integration tests for token service

---
+++ src/modules/token/token.service.int-spec.ts
@@ -0,0 +1,43 @@
+import { DatabaseService } from 'src/services/database/database.service';
+import { TokenService } from './token.service';
+import { app } from '../../../test/setup';
+import crypto from 'node:crypto';
+
+describe('TokenService', () => {
+  let tokenService: TokenService;
+  let db: DatabaseService;
+
+  beforeAll(async () => {
+    tokenService = app.get(TokenService);
+    db = app.get(DatabaseService);
+  });
+
+  describe('generateToken()', () => {
+    it('should generate token and store it', async () => {
+      const result = await tokenService.generateToken();
+      const [id, secret] = result.token.split('.');
+      const dbToken = await db.token.findUnique({ where: { id } });
+      expect(dbToken).not.toBeNull();
+      expect(dbToken!.isRevoked).toBe(false);
+      expect(dbToken!.hash).toBe(
+        crypto.createHash('sha256').update(secret!).digest('hex'),
+      );
+    });
+  });
+
+  describe('revokeToken()', () => {
+    it('updates token to revoked', async () => {
+      const token = await db.token.create({
+        data: {
+          id: 'abc',
+          hash: 'hash',
+        },
+      });
+
+      expect(token.isRevoked).toBe(false);
+      const revokedToken = await tokenService.revokeToken('abc');
+      expect(token.id).toBe(revokedToken.id);
+      expect(revokedToken.isRevoked).toBe(true);
+    });
+  });
+});
